073e4f39d023e7a34c3713ee01b4e54c6454389b
Sofs16 <134558576+Sofs16@users.noreply.github.com>
2023-06-26 18:59:17 +0100

    Calculo Variação Percentual Portugal e Brasil

--- Ficha_de_Trabalho_4.py
+++ Ficha_de_Trabalho_4.py
@@ -8,3 +8,19 @@ dados = dados[['Country Name', '1990 [YR1990]', '2000 [YR2000]']]
 
 dados_portugal = dados[dados['Country Name'] == 'Portugal']
 dados_brazil = dados[dados['Country Name'] == 'Brazil']
+
+#3. Calculo  Variação Percentual para Portugal e Brasil
+def calculo_variacao_percentual(consumo_inicial, consumo_final):
+    return (consumo_final - consumo_inicial) / consumo_inicial * 100
+
+consumo_portugal_1990 = dados_portugal['1990 [YR1990]'].values[0]
+consumo_portugal_2000 = dados_portugal['2000 [YR2000]'].values[0]
+
+consumo_brazil_1990 = dados_brazil['1990 [YR1990]'].values[0]
+consumo_brazil_2000 = dados_brazil['2000 [YR2000]'].values[0]
+
+variacao_percentual_portugal = calculo_variacao_percentual(consumo_portugal_1990, consumo_portugal_2000)
+print("A variação percentual do consumo de energia elétrica de Portugal nos últimos 10 anos é: ", variacao_percentual_portugal)
+
+variacao_percentual_brazil = calculo_variacao_percentual(consumo_brazil_1990, consumo_brazil_2000)
+print("A variação percentual do consumo de energia elétrica de Brasil nos últimos 10 anos é: ", variacao_percentual_brazil)
