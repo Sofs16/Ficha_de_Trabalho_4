f97997ed680cf2bab61bb770363853350d164c9b
Sofs16 <134558576+Sofs16@users.noreply.github.com>
2023-06-26 18:28:23 +0100

    Limpeza de Dados

--- Ficha_de_Trabalho_4.py
+++ Ficha_de_Trabalho_4.py
@@ -2,3 +2,9 @@
 import pandas as pd
 
 dados = pd.read_excel("P_Data_Extract_From_World_Development_Indicators.xlsx")
+
+#2. Limpeza de dados
+dados = dados[['Country Name', '1990 [YR1990]', '2000 [YR2000]']]
+
+dados_portugal = dados[dados['Country Name'] == 'Portugal']
+dados_brazil = dados[dados['Country Name'] == 'Brazil']
